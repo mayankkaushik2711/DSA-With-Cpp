dbe77aa5230f1ac6e28e149c706b7663caf595ac
mayankkaushik2711 <125343491+mayankkaushik2711@users.noreply.github.com>
2023-09-25 01:19:45 +0530

    Update Matrix_multiplication.cpp

--- Cpp Code/2Darray/Matrix_multiplication.cpp
+++ Cpp Code/2Darray/Matrix_multiplication.cpp
@@ -41,7 +41,7 @@ int main(){
             }     
         }
     }
-
+cout<<"multiplied_matrix\n";
     for(int i=0 ; i<2 ; i++){
         for(int j=0 ; j<2 ; j++){
             cout<<arr[i][j]<<" ";
