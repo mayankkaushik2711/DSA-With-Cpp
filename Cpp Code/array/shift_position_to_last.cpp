1981e9c8ee38a9edfb3d8c8c0007f1814dd435d4
mayank <iammayank2711@gmail.com>
2023-10-01 23:55:37 +0530

    array

---
+++ Cpp Code/array/shift_position_to_last.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+    int k=3;
+    int s=7;
+    int store=arr[6];
+
+    while(k>0){
+        int store=arr[6];
+        for (int i = 0; i < s-1; i++)
+        {
+            arr[i+1]=arr[i];
+           
+        }
+        store=arr[0];
+        k--;
+    }
+
+
+}
